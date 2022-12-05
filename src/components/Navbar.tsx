df441cf65ea784c2cf2519d2c1cb3e80a50d01ab
a1tsv <forakk01@yandex.ru>
2022-12-05 12:36:35 +0300

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Layout, Row} from "antd";
+
+export const Navbar = () => {
+   return (<div>
+         <Layout.Header>
+            <Row justify={'end'}>
+
+            </Row>
+         </Layout.Header>
+      </div>);
+};
+
